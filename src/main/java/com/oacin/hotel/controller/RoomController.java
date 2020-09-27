987ef488354cf522a722f53a62647ef97856c078
Nicollas de Oliveira <xnicobr@hotmail.com>
2020-09-26 23:13:58 -0300

    Added implementation of findAll and findByNumber at RoomRepository/RoomController

--- src/main/java/com/oacin/hotel/controller/RoomController.java
+++ src/main/java/com/oacin/hotel/controller/RoomController.java
@@ -1,8 +1,29 @@
 package com.oacin.hotel.controller;
 
+import java.util.List;
+
+import com.oacin.hotel.model.Room;
+import com.oacin.hotel.repository.RoomRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoomController {
-  
+  @Autowired
+  RoomRepository repository;
+
+  @GetMapping("/room")
+  List<Room> findAll()
+  {
+    return repository.findAll();
+  }
+
+  @GetMapping("/room/{number}")
+  Room findByNumber(@PathVariable int number)
+  {
+    return repository.findByNumber(number);
+  }
 }
--- src/main/java/com/oacin/hotel/repository/RoomRepository.java
+++ src/main/java/com/oacin/hotel/repository/RoomRepository.java
@@ -1,12 +1,14 @@
 package com.oacin.hotel.repository;
 
+import com.oacin.hotel.model.Room;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.oacin.hotel.model.Room;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
 
 @Repository
 public class RoomRepository {
@@ -21,12 +23,55 @@ public class RoomRepository {
   {
     List<Room> rooms = new ArrayList<>();
 
+    String query = "SELECT number, floor, type, capacity FROM room";
+
+    try (Connection conn = jdbcTemplate.getDataSource().getConnection()) {
+      Statement st = conn.createStatement();
+
+      ResultSet rs = st.executeQuery(query);
+
+      while(rs.next())
+      {
+        Room room = new Room();
+
+        room.setNumber(rs.getInt("number"));
+        room.setFloor(rs.getString("floor"));
+        room.setType(rs.getString("type"));
+        room.setCapacity(rs.getInt("capacity"));
+
+        rooms.add(room);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
     return rooms;
   }
 
   public Room findByNumber(int number)
   {
     Room room = new Room();
+
+    String query = String.format("SELECT number, floor, type, capacity FROM room " + 
+    "WHERE number = %s", number);
+
+    try (Connection conn = jdbcTemplate.getDataSource().getConnection()) {
+      Statement st = conn.createStatement();
+
+      ResultSet rs = st.executeQuery(query);
+
+      while(rs.next())
+      {
+        room.setNumber(rs.getInt("number"));
+        room.setFloor(rs.getString("floor"));
+        room.setType(rs.getString("type"));
+        room.setCapacity(rs.getInt("capacity"));
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
     return room;
   }
