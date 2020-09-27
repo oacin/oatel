1c0aac7df70d9812f4c2fefc202298f903423926
Nicollas de Oliveira <xnicobr@hotmail.com>
2020-09-27 01:10:18 -0300

    Added implementation of findAll at BookingRepository/BookingController

--- src/main/java/com/oacin/hotel/controller/BookingController.java
+++ src/main/java/com/oacin/hotel/controller/BookingController.java
@@ -1,8 +1,22 @@
 package com.oacin.hotel.controller;
 
+import java.util.List;
+
+import com.oacin.hotel.model.Booking;
+import com.oacin.hotel.repository.BookingRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookingController {
-  
+  @Autowired
+  BookingRepository repository;
+
+  @GetMapping("/booking")
+  List<Booking> findAll()
+  {
+    return repository.findAll();
+  }
 }
--- src/main/java/com/oacin/hotel/repository/BookingRepository.java
+++ src/main/java/com/oacin/hotel/repository/BookingRepository.java
@@ -1,5 +1,52 @@
 package com.oacin.hotel.repository;
 
+import com.oacin.hotel.model.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+
+@Repository
 public class BookingRepository {
-  
+  private final JdbcTemplate jdbcTemplate;
+
+  public BookingRepository(JdbcTemplate jdbcTemplate)
+  {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public List<Booking> findAll()
+  {
+    List<Booking> bookings = new ArrayList<>();
+
+    String query = "SELECT id, room, startDate, endDate, price, guest FROM booking";
+
+    try (Connection conn = jdbcTemplate.getDataSource().getConnection()) {
+      Statement st = conn.createStatement();
+
+      ResultSet rs = st.executeQuery(query);
+
+      while(rs.next())
+      {
+        Booking booking = new Booking();
+
+        booking.setId(rs.getInt("id"));
+        booking.setRoom(rs.getInt("room"));
+        booking.setStartDate(rs.getDate("startDate"));
+        booking.setEndDate(rs.getDate("endDate"));
+        booking.setPrice(rs.getFloat("price"));
+        booking.setGuest(rs.getString("guest"));
+
+        bookings.add(booking);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return bookings;
+  }
 }
